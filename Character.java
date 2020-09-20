6e74d58763f99f0e9c434e3e5a173fbf5f0d4b78
Erick Astorga <ag.erick@hotmail.com>
2020-09-20 15:29:55 -0600

    Creacion de clase personaje
    Este archivo contiene la clase del jugador

---
+++ Character.java
@@ -0,0 +1,11 @@
+public class Character {
+
+    //atributos
+    public int Dinero;
+    public int Vida;
+    public int Ataque;
+    public int Magia;
+    public int Defensa;
+    public int Velocidad;
+
+}
