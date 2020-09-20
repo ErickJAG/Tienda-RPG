8fcada96468aa28cb54a0a7b210092cdc42e7a2b
Esteban Granda <180401@estudiantec.cr>
2020-09-20 15:56:35 -0600

    Consumibles update

---
+++ Consumibles.java
@@ -0,0 +1,11 @@
+public class Consumibles {
+
+    //atributos
+    private int PrecioC;
+    private int PrecioV;
+    private int Ataque;
+    private int Magia;
+    private int Velocidad;
+    private int Salud;
+
+}
