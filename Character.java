a5aeba35828a7e0c09a18d1e45a8bcbb34b679c9
Esteban Granda <180401@estudiantec.cr>
2020-09-22 16:18:54 -0600

    objeto personaje

--- Character.java
+++ Character.java
@@ -10,6 +10,16 @@ public class Character {
     private int Magia;
     private int Defensa;
     private int Velocidad;
+
+    public static void main(String[] args) {
+        Character personaje = new Character();
+        personaje.Ataque= 5;
+        personaje.Vida= 5;
+        personaje.Dinero= 5000;
+        personaje.Magia= 5;
+        personaje.Defensa= 5;
+        personaje.Velocidad= 5;
+    }
     List<Armadura> inventarioDArmaduras = new ArrayList<>();
     List<Armas> inventarioArmas = new ArrayList<>();
     List<Consumibles> inventarioConsumibles = new ArrayList<>();
