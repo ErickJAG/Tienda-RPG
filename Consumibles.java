1a3cce75ddcad5db6aa267c0688bfbb7ba639c7c
Erick <ag.erick@hotmail.com>
2020-09-24 13:41:41 -0600

    Atributos arreglados en consumibles

--- Consumibles.java
+++ Consumibles.java
@@ -3,10 +3,6 @@ public class Consumibles {
     //atributos
     public int PrecioC;
     public int PrecioV;
-    public int Ataque;
-    public int Magia;
-    public int Velocidad;
-    public int Salud;
 
     //Metodo para definir los atributos de los objetos
     public static void main(String[] args) {
@@ -17,35 +13,15 @@ public class Consumibles {
         Consumibles item4 = new Consumibles();
         Consumibles item5 = new Consumibles();
         //Definicions de atributos
-        item1.Salud=0;
         item1.PrecioC=0;
         item1.PrecioV=0;
-        item1.Ataque=0;
-        item1.Velocidad=0;
-        item1.Magia=0;
-        item2.Salud=0;
         item2.PrecioC=0;
         item2.PrecioV=0;
-        item2.Ataque=0;
-        item2.Velocidad=0;
-        item2.Magia=0;
-        item3.Salud=0;
         item3.PrecioC=0;
         item3.PrecioV=0;
-        item3.Ataque=0;
-        item3.Velocidad=0;
-        item3.Magia=0;
-        item4.Salud=0;
         item4.PrecioC=0;
         item4.PrecioV=0;
-        item4.Ataque=0;
-        item4.Velocidad=0;
-        item4.Magia=0;
-        item5.Salud=0;
         item5.PrecioC=0;
         item5.PrecioV=0;
-        item5.Ataque=0;
-        item5.Velocidad=0;
-        item5.Magia=0;
     }
 }
